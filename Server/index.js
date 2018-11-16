d4684f0a7a8da1b9ea930f622ab812c68378794d
ZhrChaos <trentwhimpey@gmail.com>
2018-11-16 14:47:07 -0700

    very best

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 
 //Yo! Wassup people. Who's down for some Smash Bros?
-=======
 // This is my change for the first time on my very own branch of awesomeness. 
 let ryan = 'The very best like no one ever was!!!';
->>>>>>> 00b4f4ddbfd6a3664b9bb460607115e50c5e5daf
+
+const trent = 'The real very best.'
