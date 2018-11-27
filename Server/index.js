78372e60a471f4e2994cc6b871bb738894ea6cb0
Jon Fuchs <jonfox@Jons-MacBook-Pro.local>
2018-11-27 16:48:22 -0700

    added provider and store to index.js and installed BrowserRouter and started router setup

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
-//Yo! Wassup people. Who's down for some Smash Bros?
-// This is my change for the first time on my very own branch of awesomeness. 
-let ryan = 'The very best like no one ever was!!!';
-
-const trent = 'The real very best.'
-=======
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
@@ -42,4 +34,3 @@ massive(CONNECTION_STRING)
     app.listen(SERVER_PORT, () => {
         console.log(`Port ${SERVER_PORT} is ready to teach!!!`)
     });
->>>>>>> master
