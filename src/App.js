e708d09cf59654ab7faf180961fe27000807a1a6
Jon Fuchs <jonfox@Jons-MacBook-Pro.local>
2018-11-28 11:19:40 -0700

    finished intial routes setup, and created componments. and started sass/scss setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './Components/Home/Home'
+import UserHome from './Components/User/UserHome'
+import GameControl from './Components/Game/GameControl'
+import GameDisplay from './Components/Game/GameDisplay'
+
 
 class App extends Component {
   render() {
@@ -10,6 +14,8 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/userhome' component={UserHome} />
+          <Route path='/gamecontrol' component={GameControl} />
+          <Route path='/gamedisplay' component={GameDisplay} />
 
         </Switch>
       </BrowserRouter>
---
+++ src/Components/Game/GameControl.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+
+class GameControl extends Component{
+
+    render(){
+    
+        return(
+
+            <div>
+                <h1>Game Control where the teacher controls the game</h1>
+            </div>
+        )
+
+    }
+
+
+
+
+}
+
+
+export default GameControl;
