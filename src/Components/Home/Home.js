2543a0b7faf1eb3024a5d06ec9fa2b34c9c03238
JeffPatten <jeffspatten@gmail.com>
2018-11-29 09:23:48 -0700

    added home component, login and new user modal, and scss components folder

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,22 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Carousel } from 'antd';
+import Login from './login';
+import NewUser from './NewUser';
+import '../../scss/App.scss';
 
 
 
-class Home extends Component{
+class Home extends Component {
 
-    render(){
-    
-        return(
+    render() {
+
+        return (
 
             <div>
                 <h1>HOME</h1>
+                <nav className='lhNav'>
+                    <Login />
+                    <NewUser />
+                </nav>
+                <h1>Carousel - Wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!!!!!!!</h1>
+                <Carousel autoplay>
+                    <div><img src='https://onelittleproject.com/wp-content/uploads/2017/01/penguin-craft.jpg' alt="It's a penguin!" /></div>
+                    <div><img src='https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/sn-penguins_3.jpg?itok=rvjLuG8R' alt="It's a penguin!" /></div>
+                    <div><img src='http://themepack.me/i/c/749x468/media/g/502/emperor-penguin-theme-1.jpg' alt="It's a penguin!" /></div>
+                    <div><img src='https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/sn-penguins_3.jpg?itok=rvjLuG8R' alt="It's a penguin!" /></div>
+                </Carousel>
             </div>
         )
 
     }
-
-
-
 
 }
 
---
+++ src/Components/Home/NewUser.js
@@ -0,0 +1,163 @@
+import React, { Component } from 'react';
+import { Modal, Button, Form, Input } from 'antd';
+import '../../scss/App.scss';
+
+const FormItem = Form.Item;
+
+class NewUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            visible: false,
+            email: '',
+            password: '',
+            confirmDirty: false,
+            autoCompleteResult: []
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+        //axios request to create new user with bcrypt
+    }
+
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+      
+        return (
+            <div>
+                <Button type="primary" onClick={this.showModal}>
+                    Register
+        </Button>
+                <Modal
+                    title="Register"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormItem
+                            {...formItemLayout}
+                            label="E-mail"
+                        >
+                            {getFieldDecorator('email', {
+                                rules: [{
+                                    type: 'email', message: 'The input is not valid E-mail!',
+                                }, {
+                                    required: true, message: 'Please input your E-mail!',
+                                }],
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Password"
+                        >
+                            {getFieldDecorator('password', {
+                                rules: [{
+                                    required: true, message: 'Please input your password!',
+                                }, {
+                                    validator: this.validateToNextPassword,
+                                }],
+                            })(
+                                <Input type="password" />
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Confirm Password"
+                        >
+                            {getFieldDecorator('confirm', {
+                                rules: [{
+                                    required: true, message: 'Please confirm your password!',
+                                }, {
+                                    validator: this.compareToFirstPassword,
+                                }],
+                            })(
+                                <Input type="password" onBlur={this.handleConfirmBlur} />
+                            )}
+                        </FormItem>
+                        <FormItem {...tailFormItemLayout}>
+                            <Button type="primary" htmlType="submit">Register</Button>
+                        </FormItem>
+                    </Form>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+const WrappedRegistrationForm = Form.create()(NewUser);
+
+export default WrappedRegistrationForm
