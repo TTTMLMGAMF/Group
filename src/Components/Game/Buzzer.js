5d22268c2bfe02e857c0ad5098321b7ca0e1d352
Jon Fuchs <jonfox@Jons-MacBook-Pro.local>
2018-12-13 20:20:28 -0700

    added button styles and join game styles

--- src/Components/Game/Buzzer.js
+++ src/Components/Game/Buzzer.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 
 export default class Buzzer extends Component {
-
+constructor () {
+    super();
+    this.state = {
+        name: ''
+    }
+}
 
     render() {
         return(
             <div>
-                <button>Smash it!</button>
+                <div className='buzzerContainer'>
+                    <div className='buzzerBox'> 
+                        <h1>{this.state.name}</h1>
+                        <button>Smash it!</button>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/Components/Game/JoinGame.js
+++ src/Components/Game/JoinGame.js
@@ -5,7 +5,9 @@ export default class JoinGame extends Component {
     constructor() {
         super();
         this.state = {
-            roomCode: ''
+            roomCode: '',
+            name: '',
+            value: 0
         }
         // this.joinRoom = this.joinRoom.bind(this);
     }
@@ -19,15 +21,32 @@ export default class JoinGame extends Component {
     //   }
 
     // ${this.state.room}
+    handleColor
 
     render() {
         return(
             <div>
-                <h1>Join this game's ish</h1>
-                <p>Room Code:<input onChange={(e) => {this.setState({room: e.target.value})}} /></p>
-                {<Link to={`/buzzer`} 
-                target="_blank"><button>Join Game</button></Link>}
+                <div className='joinContainer'>
+                    <div className='inputBox'>
+                        <h1>Join the game</h1>
+                        <p>What is your name:</p>
+                        <input/>
+                        <p>What is your room code:</p>
+                        <input onChange={(e) => {this.setState({room: e.target.value})}} />
+                        {/* <p>What is your favorite color?</p> */}
+                        {/* <select onChange={}>
+                            <option value={1}>Red</option>
+                            <option value={2}>Yellow</option>
+                            <option value={3}>Blue</option>
+                            <option value={4}>Green</option>
+                            <option value={5}>Orange</option>
+                        </select> */}
+                        {<Link to={`/buzzer/${this.state.roomCode}`} 
+                        target="_blank"><button>SUBMIT</button></Link>}
+                    </div>
+                </div>
             </div>
+
         )
     }
 }
